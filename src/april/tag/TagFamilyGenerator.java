556975515245399b1d52c2faa5d634f5763d6506
kralo <kralo@users.noreply.github.com>
2017-07-18 01:28:31 +0200

    convert to static class, different optimizations

--- src/april/tag/TagFamilyGenerator.java
+++ src/april/tag/TagFamilyGenerator.java
@@ -1,29 +1,28 @@
 package april.tag;
 
-import java.io.*;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Random;
 
 public class TagFamilyGenerator {
-	final int nbits;
-	final int minhamming;
-	final int mincomplexity;
-	final int d;
-	final long V0;
+	private final static int nbits = 36;
+	private final static int minhamming = 9;
+	private final static int mincomplexity = 10;
+	private final static int d = (int) Math.sqrt(nbits);
+	private static long V0;
+	private final static long maxnum = (1L << nbits);
+	private final static long maxnumm1 = maxnum - 1;
 
-	long starttime;
+	private static long starttime;
 
-	long[] codelist = new long[64];
-	int rotlim = -1; // write next entry at index
+	private static long[] codelist = new long[0];
+	private static int rotlim = -1; // write next entry at index
 
-	static final long PRIME = 982451653;
-
-	public TagFamilyGenerator(int nbits, int minhamming, int mincomplexity, long V0) {
-		this.nbits = nbits;
-		this.minhamming = minhamming;
-		this.mincomplexity = mincomplexity;
-		this.d = (int) Math.sqrt(nbits);
-		this.V0 = V0;
-
+	private static final long PRIME = 982451653;
+	
+	static {
 		if (d * d != nbits) {
 			System.out.println("WARNING: nbits is not a square. This code may do something stupid.\n");
 		}
@@ -44,14 +43,15 @@ public class TagFamilyGenerator {
 	}
 
 	public static void main(String args[]) {
-		if (args.length < 2) {
-			System.out.printf("usage: <nbits> <minhammingdistance> <mincomplexity> <number of rounds>\n");
-			System.out.printf("(For all standard tags, nbits is a square.)\n");
-			return;
-		}
+		// if (args.length < 2) {
+		// System.out.printf("usage: <nbits> <minhammingdistance>
+		// <mincomplexity> <number of rounds>\n");
+		// System.out.printf("(For all standard tags, nbits is a square.)\n");
+		// return;
+		// }
 
-		int nbits = Integer.parseInt(args[0]);
-		int minhamming = Integer.parseInt(args[1]);
+		// int nbits = Integer.parseInt(args[0]);
+		// int minhamming = Integer.parseInt(args[1]);
 
 		// default complexity is a function of the nbits. Values
 		// before were tuned by hand so they were "reasonable".
@@ -64,11 +64,12 @@ public class TagFamilyGenerator {
 		// 36 | 10
 
 		// This is approximately: complexity = 0.3 * nbits.
-		int reccomplexity = Math.min(10, nbits / 3);
+		// int reccomplexity = Math.min(10, nbits / 3);
 
-		int mincomplexity = args.length > 2 ? Integer.parseInt(args[2]) : reccomplexity;
+		// int mincomplexity = args.length > 2 ? Integer.parseInt(args[2]) :
+		// reccomplexity;
 
-		int maxruns = args.length > 3 ? Integer.parseInt(args[3]) : 1;
+		int maxruns = args.length > 0 ? Integer.parseInt(args[0]) : 1;
 
 		int bestCountCodes = 0;
 		// begin our search at a random position to avoid any bias
@@ -81,10 +82,11 @@ public class TagFamilyGenerator {
 
 		for (int i = 0; i < maxruns; i++) {
 			long V0 = rand.nextLong();
-			TagFamilyGenerator tfg = new TagFamilyGenerator(nbits, minhamming, mincomplexity, V0);
-			TagFamily res = tfg.compute();
+			codelist = new long[0];
+			rotlim = -1;
+			TagFamily res = TagFamilyGenerator.compute(V0);
 			if (res.codes.length > bestCountCodes) {
-				tfg.report("_" + V0);
+				TagFamilyGenerator.report("_" + V0);
 				bestCountCodes = res.codes.length;
 			}
 			System.out.format("found family with %d codes, best is now %d !", res.codes.length, bestCountCodes);
@@ -93,39 +95,54 @@ public class TagFamilyGenerator {
 		System.out.println("finished.");
 	}
 
-	boolean isCodeOkay(final long v) {
-		// The tag must be different from itself when rotated.
-		final long rv1 = TagFamily.rotate90(v, d);
-		final long rv2 = TagFamily.rotate90(rv1, d);
-		final long rv3 = TagFamily.rotate90(rv2, d);
+	private static long rotate90(final long w) {
+		long wr = 0;
 
-		if (!hammingDistanceAtLeast(v ^ rv1, minhamming) || !hammingDistanceAtLeast(v ^ rv2, minhamming) || !hammingDistanceAtLeast(rv1 ^ rv2, minhamming)
-				|| !hammingDistanceAtLeast(rv1 ^ rv3, minhamming) || !hammingDistanceAtLeast(rv2 ^ rv3, minhamming) || !hammingDistanceAtLeast(v ^ rv3, minhamming)) {
+		for (int r = d - 1; r >= 0; r--) {
+			for (int c = 0; c < d; c++) {
+				int b = r + d * c;
+
+				wr = wr << 1;
+
+				if ((w & (1L << b)) != 0)
+					wr |= 1;
+
+			}
+		}
+		return wr;
+	}
+
+	private static boolean isCodeOkay(final long v) {
+		// The tag must be different from itself when rotated.
+		final long rv1 = rotate90(v);
+		final long rv2 = rotate90(rv1);
+		final long rv3 = rotate90(rv2);
+
+		if (hammingDistanceTooLow(v ^ rv1) || hammingDistanceTooLow(v ^ rv2) || hammingDistanceTooLow(rv1 ^ rv2) || hammingDistanceTooLow(rv1 ^ rv3)
+				|| hammingDistanceTooLow(rv2 ^ rv3) || hammingDistanceTooLow(v ^ rv3)) {
 			return false;
 		}
 
 		// tag (and its rotations) must be different from other tags.
 		for (int i = 0; i <= rotlim; i++) {
-			final long toCheck = codelist[i];
-			if (!hammingDistanceAtLeast(v ^ toCheck, minhamming) || !hammingDistanceAtLeast(rv1 ^ toCheck, minhamming) || !hammingDistanceAtLeast(rv2 ^ toCheck, minhamming)
-					|| !hammingDistanceAtLeast(rv3 ^ toCheck, minhamming)) {
-				{
-					return false;
-				}
+			if (hammingDistanceTooLow(v ^ codelist[i]) || hammingDistanceTooLow(rv1 ^ codelist[i]) || hammingDistanceTooLow(rv2 ^ codelist[i])
+					|| hammingDistanceTooLow(rv3 ^ codelist[i])) {
+				return false;
 			}
 		}
 
 		// tag must be reasonably complex
-		if (computeComplexity(v, d) < mincomplexity) {
+		if (computeComplexity(v) < mincomplexity) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public TagFamily compute() {
-		assert (codelist == null);
+	public static TagFamily compute(final long V0) {
+		assert (codelist.length == 0);
 		starttime = System.currentTimeMillis();
+		TagFamilyGenerator.V0 = V0;
 
 		long lastreporttime = starttime;
 		long lastprogresstime = starttime;
@@ -134,7 +151,7 @@ public class TagFamilyGenerator {
 		System.out.printf("Using only one thread. V0=%d\n", V0);
 
 		long iter = 0;
-		long chunksize = 5000000;
+		long chunksize = 500000;
 
 		// compute v = V0 + PRIME * iter0,
 		// being very careful about overflow.
@@ -146,11 +163,11 @@ public class TagFamilyGenerator {
 			while (M > 0) {
 				if ((M & 1) > 0) {
 					v += acc;
-					v &= ((1L << nbits) - 1);
+					v &= (1L << nbits) - 1;
 				}
 
 				acc *= 2;
-				acc &= ((1L << nbits) - 1);
+				acc &= (1L << nbits) - 1;
 				M >>= 1;
 			}
 		}
@@ -173,7 +190,7 @@ public class TagFamilyGenerator {
 					chunksize = (long) (rate * 5);
 					chunksize = Math.min(chunksize, (1L << nbits) - iter);
 					double secremaining = ((long) (1L << nbits) - iter) / rate;
-					System.out.printf("%8.4f%% , iter: %d,  codes: %-5d (%.0f iters/sec, %.2f minutes = %.2f hours)    chunksize: %d\r", donepercent, iter, codelist.length, rate,
+					System.out.printf("%8.4f%% , iter: %d,  codes: %-5d (%.0f iters/sec, %.2f minutes = %.2f hours)    chunksize: %d\r", donepercent, iter, rotlim, rate,
 							secremaining / (60.0), secremaining / 3600.0, chunksize);
 
 					lastprogresstime = now;
@@ -181,38 +198,30 @@ public class TagFamilyGenerator {
 				}
 			}
 
-			long bestCode = -1;
-			{
-
-				long iterStart = iter;
-				while (iter < iterStart + chunksize) {
-					iter++;
-					v += PRIME; // big prime.
-					v &= ((1L << nbits) - 1);
-					if (isCodeOkay(v)) {
-						bestCode = v;
-						break;
+			for (long iterstop = iter + chunksize; iter < iterstop; iter++) {
+				v += PRIME; // big prime.
+				v &= maxnumm1;
+				if (isCodeOkay(v)) {
+					if (rotlim + 1 >= codelist.length) {
+						long[] newrot = new long[codelist.length + 1024];
+						System.arraycopy(codelist, 0, newrot, 0, codelist.length);
+						codelist = newrot;
 					}
+					// System.out.printf("0x%0" + ((int) Math.ceil(nbits /
+					// 4)) +
+					// "xL\r\n", v);
+					codelist[++rotlim] = v;
+					// break;
 				}
-
-			}
-
-			if (bestCode >= 0) {
-				if (rotlim + 1 >= codelist.length) {
-					long[] newrot = new long[codelist.length + 1024];
-					System.arraycopy(codelist, 0, newrot, 0, codelist.length);
-					codelist = newrot;
-				}
-				codelist[++rotlim] = bestCode;
 			}
 
 		}
 
-		long[] codes = new long[rotlim+1];
+		long[] codes = new long[rotlim + 1];
 		System.arraycopy(codelist, 0, codes, 0, codes.length);
 		TagFamily tagFamily = new TagFamily(nbits, minhamming, codes);
 
-		if (true) {
+		if (false) {
 			try {
 				tagFamily.writeAllImages("/tmp");
 				tagFamily.writeAllImagesMosaic("/tmp/mosaic.png");
@@ -224,20 +233,20 @@ public class TagFamilyGenerator {
 		return tagFamily;
 	}
 
-	void report(String appendName) {
+	static void report(String appendName) {
 
 		String cname = String.format("Tag%dh%dc%d", nbits, minhamming, mincomplexity);
 		PrintStream out = null;
 		try {
-			out = new PrintStream(new FileOutputStream("/tmp/" + cname + appendName + ".java", false));
+			out = new PrintStream(new FileOutputStream("" + cname + appendName + ".java", false));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		long[] codes = new long[rotlim+1];
+		long[] codes = new long[rotlim + 1];
 		System.arraycopy(codelist, 0, codes, 0, codes.length);
-		
+
 		int hds[] = new int[nbits + 1];
 		// int hdtotal = 0;
 
@@ -322,8 +331,8 @@ public class TagFamilyGenerator {
 		return v;
 	}
 
-	static int computeComplexity(long v, int d) {
-		int a[][] = new int[d][d];
+	private static int computeComplexity(long v) {
+		final int a[][] = new int[d][d];
 
 		for (int y = 0; y < d; y++) {
 			for (int x = 0; x < d; x++) {
@@ -340,8 +349,8 @@ public class TagFamilyGenerator {
 		return Long.bitCount(a ^ b);
 	}
 
-	public static final boolean hammingDistanceAtLeast(final long w, final int minval) {
-		return Long.bitCount(w) >= minval;
+	private static final boolean hammingDistanceTooLow(final long w) {
+		return Long.bitCount(w) < minhamming;
 	}
 
 	/**
@@ -356,12 +365,12 @@ public class TagFamilyGenerator {
 	 *
 	 * @param foo
 	 **/
-	static int computeComplexity(final int d[][]) {
+	private static int computeComplexity(final int d[][]) {
 		final boolean verbose = false;
 
 		final int width = d[0].length, height = d.length;
 
-		int out[][] = new int[height][width];
+		final int out[][] = new int[height][width];
 		int numrectangles = 0;
 
 		// initialize output to invalid color.
